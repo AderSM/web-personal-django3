8ce030dde8b8f8aeb6f45735d723de1575a04ceb
AderSM <fredericksuarez@outlook.es>
2020-07-17 21:07:24 -0500

    Proyecto añadido webPersonal

---
+++ webpersonal/core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, HttpResponse
+
+html_base ="""
+    <style>
+    body{
+        background-color: linen;
+    }
+    </style>
+
+    <h1>Mi web Personal</h1>
+    <ul>
+        <li><a href ="/">Portada</a></li>
+        <li><a href ="/about/">Acerca de...</a></li>
+        <li><a href ="/portfolio/">Portafolio</a></li>
+        <li><a href ="/contact/">Contacto</a></li>
+    </ul>
+"""
+
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
